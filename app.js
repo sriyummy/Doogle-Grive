ea75e79843ff5a7135d2910a861aa99ebdb8946a
sriyummy <sriyam2004@gmail.com>
2025-04-07 15:29:55 +0530

    Added authentication using JWT and cookies

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require ('express');
 const dotenv = require ('dotenv');
+const cookieParser = require ('cookie-parser');
 const userRouter = require ('./routes/user.routes');
 const connectToDB = require ('./config/db');
 
@@ -8,6 +9,7 @@ const app = express();
 connectToDB();
 
 app.set ('view engine', 'ejs');
+app.use (cookieParser ());
 app.use (express.json());
 app.use (express.urlencoded ({extended: true}));
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,6 +2,8 @@ const express = require ('express');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 const userModel = require('../models/user.model');
+const bcrypt = require ('bcrypt');
+const jwt = require ('jsonwebtoken');
 
 router.get ('/test', (req, res) => {
     res.send ('Test route is working');
@@ -24,14 +26,61 @@ router.post ('/register',
         });
     }
     const {email, username, password } = req.body
+    const hashPassword = await bcrypt.hash (password, 10);
 
     const newUser = await userModel.create ({
         email,
         username,
-        password
+        password: hashPassword
     });
 
     res.json (newUser);
 });
+
+router.get ('/login', (req, res) => {
+    res.render ('login');
+});
+
+router.post ('/login',
+    body('username').trim().isLength({min:3}),
+    body('password').trim().isLength({min:5}), async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()){
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'Invalid Data'
+            });
+        }
+
+        const { username, password } = req.body;
+
+        const user = await userModel.findOne ({
+            username: username
+        });
+
+        if (!user) {
+            return res.status (400).json ({
+                message: 'Username or password is incorrect'
+            });
+        }
+
+        const isMatch = await bcrypt.compare (password, user.password);
+
+        if (!isMatch) {
+            return res.status (400).json ({
+                message: 'Username or password is incorrect'
+            });
+        }
+
+        const token = jwt.sign ({
+            userId: user._id,
+            email: user.email,
+            username: user.username
+        },
+        process.env.JWT_SECRET);
+
+        res.cookie ('token', token);
+        res.send ('Loggen In ');
+ });
 
 module.exports = router;
