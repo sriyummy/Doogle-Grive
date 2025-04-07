2c762ca6b959f1c2e7e59cdff814f2f3713ea328
sriyummy <sriyam2004@gmail.com>
2025-04-07 20:23:52 +0530

    All middlewares and routes added, and functionalities are final with firebase authentication

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,14 +1,15 @@
 const express = require ('express');
+const authMiddleware = require ('../middlewares/auth');
 
 const router = express.Router();
 const upload = require ('../config/multer.config');
 const fileModel = require ('../models/files.models');
 
-router.get ('/home', (req, res) => {
+router.get ('/home', authMiddleware, (req, res) => {
     res.render ('home');
 });
 
-router.post ('/upload', upload.single ('file'), async (req, res) => {
+router.post ('/upload', authMiddleware, upload.single ('file'), async (req, res) => {
     const newFile = await fileModel.create ({
         path: req.file.path,
         originalname: req.file.originalname
